115ff8bb2dacde29e0348887908247d3eae168be
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-09-13 18:13:32 +0300

    make build

---
+++ index.804b5513.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".icon-menu");e&&e.addEventListener("click",function(e){document.documentElement.classList.toggle("menu-open")}),document.addEventListener("click",function(n){n.target!==e&&document.documentElement.classList.contains("menu-open")&&document.documentElement.classList.remove("menu-open")});
+//# sourceMappingURL=index.804b5513.js.map
